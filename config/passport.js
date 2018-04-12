138b153cd5a417d96d39a6b93640997ebe300a57
juancarlucci <juancarloscollins@gmail.com>
2018-04-12 15:20:09 -0700

    change calbackURL for passport to heroku

--- config/passport.js
+++ config/passport.js
@@ -30,7 +30,7 @@ module.exports = function(passport){
   passport.use(new GitHubStrategy({
     clientID      : process.env.GITHUB_CLIENT_ID,
     clientSecret  : process.env.GITHUB_CLIENT_SECRET,
-    callbackURL   : "http://localhost:3000/auth/github/callback",
+    callbackURL   : "https://codeinsight.herokuapp.com/auth/github/callback",
     proxy         : true
   }, function(accessToken, refreshToken, profile, done) {
 
